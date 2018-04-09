5bb7828fce1070ae6783082b8121ac5ad509de39
Christiebelle <cslester@outlook.com>
2018-04-09 15:35:10 +0100

    pseudocoded out the steps for adding a cat programmatically

---
+++ public/app.js
@@ -0,0 +1,10 @@
+const app = function(){
+  //1. Create the parent container and it's class
+  let ul = document.createElement("ul");
+  ul.classList.add("cat");
+  //2. Create the first child - in this case; the blockquoute
+  //3. Create the cite element and it's text
+  //4. Append cite to the blockquote
+  //5. Append the blockquote to the article
+  //6. Insert the article to the list of quotes
+};
