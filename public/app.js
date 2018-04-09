e35e48e64d6ef3543219e31aebc2fecf771576c2
Christiebelle <cslester@outlook.com>
2018-04-09 15:51:57 +0100

    first attempt at filling in the psuedocoded steps for adding a cat.

--- public/app.js
+++ public/app.js
@@ -2,8 +2,15 @@ const app = function(){
   //1. Create the parent container and it's class
   let ul = document.createElement("ul");
   ul.classList.add("cat");
-  //2. Create the first child - in this case; the blockquoute
-  //3. Create the cite element and it's text
+  //2. Create the first child - in this case; the li
+  let li1 = document.createElement("li");
+  li1.innerText = "Name: MungoJerry";
+  //3. Create the next element and it's text
+  let li2 = document.createElement("li");
+  li2.innerText = "Favourite food: haddock";
+  //3. Create the next element and it's text
+  let li3 = document.createElement("li");
+  li3.innerHTML = "<img width="500" src=http://image.wikifoundry.com/image/1/XOVwuOEBK29dn95OeAp08g64672/GW303H303>" 
   //4. Append cite to the blockquote
   //5. Append the blockquote to the article
   //6. Insert the article to the list of quotes
