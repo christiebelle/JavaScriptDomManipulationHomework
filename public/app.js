daadc518c7b7a0747f7285347fc7b1bf3fb8603e
Christiebelle <cslester@outlook.com>
2018-04-09 16:03:26 +0100

    added a line to invoke the app.js on window loading

--- public/app.js
+++ public/app.js
@@ -10,12 +10,15 @@ const app = function(){
   li2.innerText = "Favourite food: haddock";
   //4. Create the next element and it's text
   let li3 = document.createElement("li");
-  li3.innerHTML = "<img width="500" src=http://image.wikifoundry.com/image/1/XOVwuOEBK29dn95OeAp08g64672/GW303H303>"
+  li3.innerHTML = "<img width='500' src='http://image.wikifoundry.com/image/1/XOVwuOEBK29dn95OeAp08g64672/GW303H303'>"
   //5. Append the li to the ul
   ul.appendChild(li3);
   ul.appendChild(li2);
   ul.appendChild(li1);
   //6. Insert the ul to the list of cats
-  let cats = document.querySelector(#cats);
+  let cats = document.querySelector("#cats");
   cats.appendChild(ul);
+  debugger;
 };
+
+window.onload = app;
