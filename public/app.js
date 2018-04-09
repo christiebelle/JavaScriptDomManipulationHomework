b92dba6eb6dbdc85b2cec1124ad6ef51d2469e4d
Christiebelle <cslester@outlook.com>
2018-04-09 15:54:38 +0100

    elements for creating a cat appended to their respective classes

--- public/app.js
+++ public/app.js
@@ -8,10 +8,14 @@ const app = function(){
   //3. Create the next element and it's text
   let li2 = document.createElement("li");
   li2.innerText = "Favourite food: haddock";
-  //3. Create the next element and it's text
+  //4. Create the next element and it's text
   let li3 = document.createElement("li");
-  li3.innerHTML = "<img width="500" src=http://image.wikifoundry.com/image/1/XOVwuOEBK29dn95OeAp08g64672/GW303H303>" 
-  //4. Append cite to the blockquote
-  //5. Append the blockquote to the article
-  //6. Insert the article to the list of quotes
+  li3.innerHTML = "<img width="500" src=http://image.wikifoundry.com/image/1/XOVwuOEBK29dn95OeAp08g64672/GW303H303>"
+  //5. Append the li to the ul
+  ul.appendChild(li3);
+  ul.appendChild(li2);
+  ul.appendChild(li1);
+  //6. Insert the ul to the list of cats
+  let cats = document.querySelector(#cats);
+  cats.appendChild(ul);
 };
